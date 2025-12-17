f1b37996974a2fae8f1dae1432ff275c4681cee6
dZev1 <diegozevallos04@gmail.com>
2025-12-16 23:09:16 -0300

    hello

--- automata/finite_automata/nfa_test.go
+++ automata/finite_automata/nfa_test.go
@@ -7,29 +7,19 @@ import (
 
 func TestNFA_AddState(t *testing.T) {
 	tests := []struct {
+		nfa *NFA[int]
 		name  string
 		state int
 		final bool
 	}{
 		{
-			name:  "Add a non-final state",
-			state: 4,
-			final: false,
-		},
-		{
-			name:  "Add a final state",
-			state: 4,
+			nfa: &NFA[int]{},
+			name: "Add state to empty automaton",
+			state: 0,
 			final: true,
 		},
 		{
-			name:  "Add an already added state",
-			state: 3,
-			final: true,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			nfa := NFA[int]{
+			nfa: &NFA[int]{
 				States:   set.Set[int]{0: {}, 1: {}, 2: {}, 3: {}},
 				Alphabet: set.Set[string]{"a": {}, "b": {}},
 				Transitions: map[int]map[string]set.Set[int]{
@@ -40,7 +30,49 @@ func TestNFA_AddState(t *testing.T) {
 				},
 				InitialState: 0,
 				FinalStates:  set.Set[int]{3: {}},
-			}
+			},
+			name:  "Add a non-final state",
+			state: 4,
+			final: false,
+		},
+		{
+			nfa: &NFA[int]{
+				States:   set.Set[int]{0: {}, 1: {}, 2: {}, 3: {}},
+				Alphabet: set.Set[string]{"a": {}, "b": {}},
+				Transitions: map[int]map[string]set.Set[int]{
+					0: {"a": {1: {}}, "b": {2: {}}},
+					1: {"a": {0: {}}, "b": {3: {}}},
+					2: {"a": {2: {}}, "b": {2: {}}},
+					3: {"a": {2: {}}, "b": {2: {}}},
+				},
+				InitialState: 0,
+				FinalStates:  set.Set[int]{3: {}},
+			},
+			name:  "Add a final state",
+			state: 4,
+			final: true,
+		},
+		{
+			nfa: &NFA[int]{
+				States:   set.Set[int]{0: {}, 1: {}, 2: {}, 3: {}},
+				Alphabet: set.Set[string]{"a": {}, "b": {}},
+				Transitions: map[int]map[string]set.Set[int]{
+					0: {"a": {1: {}}, "b": {2: {}}},
+					1: {"a": {0: {}}, "b": {3: {}}},
+					2: {"a": {2: {}}, "b": {2: {}}},
+					3: {"a": {2: {}}, "b": {2: {}}},
+				},
+				InitialState: 0,
+				FinalStates:  set.Set[int]{3: {}},
+			},
+			name:  "Add an already added state",
+			state: 3,
+			final: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			nfa := tt.nfa
 			previousSize := nfa.size()
 			err := nfa.AddState(tt.state, tt.final)
 
@@ -56,3 +88,19 @@ func TestNFA_AddState(t *testing.T) {
 		})
 	}
 }
+
+/*
+func TestNFA_AddTransition(t *testing.T) {
+	tests := []struct {
+		nfa *NFA[int]
+		name string
+		fromState int
+		toState int
+		symbol string	
+	}{
+		{
+			nfa 
+		}
+	}
+}
+	*/
