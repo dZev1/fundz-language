6e8dfbf5049199f8f5ed13d88b2d55b83529d23c
dZev1 <diegozevallos04@gmail.com>
2025-12-22 01:39:30 -0300

    did not want to work with finite automata, sowwy

--- todo.md
+++ todo.md
@@ -2,27 +2,8 @@
 
 ## LEXER
 
-- [x] DFA implementation
-  - [x] Minimization
-  - [x] Add transitions
-  - [x] Add states
-- [x] NFA implementation
-  - [x] Determinization
-  - [x] Add transitions
-  - [x] Add states
-- [x] Regular expression toNFA implementation.
-  - [x] ∅.
-  - [x] λ.
-  - [x] a.
-  - [x] r|s.
-  - [x] rs.
-  - [x] r*.
-  - [x] r+.
-- [ ] Check syntax and grammar for the language
-- [ ] Define tokens
-- [ ] Automata for every token.
-- [ ] Unite all automata into one single minimal automaton.
-- [ ] Recognize each token via the automaton.
+- [x] Tokenization
+- [ ] Lexer structure
 
 ## PARSER
 
---
+++ token/token.go
@@ -0,0 +1,85 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	// Special Tokens
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + Literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+	RARROW   = "->"
+	LARROW   = "<-"
+	DRARROW  = "=>"
+
+	LT = "<"
+	LEQ = "<="
+	GT = ">"
+	GEQ = ">="
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// Delimiters
+	COMMA        = ","
+	DOT          = "."
+	SEMICOLON    = ";"
+	COLON        = ":"
+	DOUBLE_COLON = "::"
+	
+
+	LPAREN   = "("
+	RPAREN   = ")"
+	LBRACE   = "{"
+	RBRACE   = "}"
+	LBRACKET = "["
+	RBRACKET = "]"
+
+	// Keywords
+	UNDERSCORE = "_"
+	LET        = "LET"
+	TRUE       = "TRUE"
+	FALSE      = "FALSE"
+	IF         = "IF"
+	THEN       = "THEN"
+	ELSE       = "ELSE"
+	BOOL_TYPE  = "BOOL_TYPE"
+	INT_TYPE   = "INT_TYPE"
+	CHAR_TYPE  = "CHAR_TYPE"
+	FLOAT_TYPE = "FLOAT_TYPE"
+)
+
+var keywords = map[string]TokenType{
+	"let":        LET,
+	"true":       TRUE,
+	"false":      FALSE,
+	"if":         IF,
+	"then":       THEN,
+	"else":       ELSE,
+	"bool":       BOOL_TYPE,
+	"int":        INT_TYPE,
+	"char":       CHAR_TYPE,
+	"float":      FLOAT_TYPE,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
